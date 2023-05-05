d70f0359277fc47b2231e74e867d3225b36f2a4f
Gustavo-Ragazzi <ragazzi@hotmail.com.br>
2023-05-05 19:30:48 -0300

    adding submit button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+export default function Button(props: any) {
+    return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-1/3 rounded-full self-center"
+            type={props.type}
+        >
+            {props.text}
+        </button>
+    )
+}
--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -1,6 +1,6 @@
 export default function InputForm(props: any) {
     return (
-        <label>
+        <label className="flex flex-col">
             <h3 className="text-white pl-4">{props.label}</h3>
             <input className="w-full h-12 bg-gray-500 px-3 text-white rounded-full" 
                 type={props.type}
