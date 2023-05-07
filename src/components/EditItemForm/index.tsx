5e06d7ca519b321c78441ec6027d6d20d7126b00
Gustavo-Ragazzi <ragazzi@hotmail.com.br>
2023-05-07 12:37:19 -0300

    adding new edit form

---
+++ src/components/EditItemForm/index.tsx
@@ -0,0 +1,95 @@
+import { useState } from "react"
+import Button from "../Button"
+import InputForm from "../InputForm"
+import { FaTimes } from "react-icons/fa"
+
+interface Item {
+    nome: string;
+    marca: string;
+    preco: number;
+    categoria: string;
+    qnt: number;
+}
+interface EditItemFormProps {
+    handlePost: (item: Item) => void;
+    onClose: () => void;
+}
+
+export default function EditItemForm(props: EditItemFormProps) {
+    const [isFormVisible, setIsFormVisible] = useState(true);
+    const [nome, setNome] = useState("");
+    const [marca, setMarca] = useState("");
+    const [preco, setPreco] = useState<number>(0);
+    const [categoria, setCategoria] = useState("");
+    const [qnt, setQnt] = useState<number>(0);
+
+    const hideForm = () => {
+        setIsFormVisible(false);
+        props.onClose();
+    };
+
+    const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const item: Item = {nome, marca, preco, categoria, qnt};
+        // props.handlePost(item);
+        console.log(item)
+    }
+
+    return (
+        <>
+            {isFormVisible && (
+                <div id="add-form" className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-600 rounded-md p-6 shadow-lg w-1/3">
+                    <button className="absolute top-0 right-0 p-2 hover:bg-gray-400" onClick={hideForm}>
+                        <FaTimes />
+                    </button>
+                    <form className="flex flex-col gap-5 justify-center" onSubmit={handleEdit}>
+                        <InputForm
+                            label={"Nome"}
+                            type={"text"}
+                            name={"nome"}
+                            placeholder={"Ryzen 5 5600x"}
+                            required={true}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNome(e.target.value)}
+                        />
+                        <InputForm
+                            label={"Marca"}
+                            type={"text"}
+                            name={"marca"}
+                            placeholder={"Asus"}
+                            required={true}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMarca(e.target.value)}
+                        />
+                        <InputForm
+                            label={"Preço"}
+                            type={"number"}
+                            step={"any"}
+                            name={"preco"}
+                            min={0}
+                            placeholder={"1399.99"}
+                            required={true}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPreco(parseFloat(e.target.value))}
+                        />
+                        <InputForm
+                            label={"Categoria"}
+                            type={"text"}
+                            name={"categoria"}
+                            placeholder={"CPU"}
+                            required={true}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCategoria(e.target.value)}
+                        />
+                        <InputForm
+                            label={"Qnt"}
+                            type={"number"}
+                            name={"qnt"}
+                            min={0}
+                            placeholder={"5"}
+                            required={true}
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQnt(parseFloat(e.target.value))}
+                        />
+                        <Button text={"Enviar"} type={"submit"}>Editar</Button>
+                    </form>
+                </div>
+            )}
+        </>
+    )
+}
--- src/components/ResultTable/ResultTable.tsx
+++ src/components/ResultTable/ResultTable.tsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import { BsTrash3Fill, BsPenFill } from "react-icons/bs";
 import { FcPlus } from "react-icons/fc";
 import AddNewItemForm from "../AddNewItemForm";
+import EditItemForm from "../EditItemForm";
 
 export default function ResultTable (props: any) {
     const [result, setResult] = useState<any[]>([]);
-    const [showForm, setShowForm] = useState(false);
+    const [showAddForm, setAddShowForm] = useState(false);
+    const [showEditForm, setEditShowForm] = useState(false);
 
     useEffect(() => {
         async function loadObject() {
@@ -24,7 +26,7 @@ export default function ResultTable (props: any) {
                         <th className="text-left px-4">Preço</th>
                         <th className="text-left px-4">Categoria</th>
                         <th className="text-left px-4">Qnt</th>
-                        <th className="text-right" onClick={() => setShowForm(true)}><button><FcPlus className="text-3xl"/></button></th>
+                        <th className="text-right" onClick={() => setAddShowForm(true)}><button><FcPlus className="text-3xl"/></button></th>
                     </tr>
                 </thead>
                 <tbody key={"ResultTable"}>
@@ -37,7 +39,7 @@ export default function ResultTable (props: any) {
                             <td key={item.qnt} className="text-left px-4 py-2">{item.qnt}</td>
                             <td key={item + "icons"} className="text-right px-4 py-2">
                                 <div key={item + "div"}  className="flex items-center justify-end gap-3">
-                                    <button onClick={() => setShowForm(true)} id={"edit" + index} className="hover:text-green-600"><BsPenFill className="align-middle"/></button>
+                                    <button onClick={() => setEditShowForm(true)} id={"edit" + index} className="hover:text-green-600"><BsPenFill className="align-middle"/></button>
                                     <button onClick={() => props.handleDelete(item.id)} id={"delete" + item.id} className="hover:text-red-600"><BsTrash3Fill className="align-middle"/></button>
                                 </div>
                             </td>
@@ -45,7 +47,8 @@ export default function ResultTable (props: any) {
                     ))}
                 </tbody>
             </table>
-            {showForm && <AddNewItemForm handlePost={props.handlePost} onClose={() => setShowForm(false)} />}
+            {showAddForm && <AddNewItemForm handlePost={props.handlePost} onClose={() => setAddShowForm(false)} />}
+            {showEditForm && <EditItemForm handlePost={props.handlePost} onClose={() => setEditShowForm(false)} />}
         </>
     )
 }
