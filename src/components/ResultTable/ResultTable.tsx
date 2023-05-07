f02340ca2b9e8b45acf021dd6a6e97cbcd81f6aa
Gustavo-Ragazzi <ragazzi@hotmail.com.br>
2023-05-07 01:52:16 -0300

    adding hover style in buttons

--- src/components/ResultTable/ResultTable.tsx
+++ src/components/ResultTable/ResultTable.tsx
@@ -37,8 +37,8 @@ export default function ResultTable (props: any) {
                             <td key={item.qnt} className="text-left px-4 py-2">{item.qnt}</td>
                             <td key={item + "icons"} className="text-right px-4 py-2">
                                 <div key={item + "div"}  className="flex items-center justify-end gap-3">
-                                    <button id={"edit" + index}><BsPenFill   className="align-middle"/></button>
-                                    <button onClick={() => props.handleDelete(item.id)} id={"delete" + item.id}><BsTrash3Fill className="align-middle"/></button>
+                                    <button id={"edit" + index} className="hover:text-green-600"><BsPenFill className="align-middle"/></button>
+                                    <button onClick={() => props.handleDelete(item.id)} id={"delete" + item.id} className="hover:text-red-600"><BsTrash3Fill className="align-middle"/></button>
                                 </div>
                             </td>
                         </tr>
